f4451ccd6faa05dcd38b206eb470c634d72d5587
Jeremy Holt <jeremy.holt@kaseya.com>
2017-07-15 19:55:28 -0700

    Test

--- wp-content/themes/kaseya/index.php
+++ wp-content/themes/kaseya/index.php
@@ -1,13 +1,9 @@
 <?php get_header(); ?>
 
-<p>
-	Is this live?
-</p>
-
 		<section id="content">
 			<div class="promo promo-full">
 				<div class="container clearfix">
-					<h3>Let's Talk about getting more out of your day and growing your IT business</h3>
+					<h3>Let's Talk about getting more out of your DAY!!! and growing your IT business</h3>
 					<span>Find out why thousands of IT departments and Managed Service Providers (MSPs) use Kaseya solutions every day to get more IT done.</span>
 					<a href="#" class="button button-3d button-large button-rounded button-amber lets-talk-btn"><i class="icon-comments-alt"></i> Let's Talk</a>
 				</div>
